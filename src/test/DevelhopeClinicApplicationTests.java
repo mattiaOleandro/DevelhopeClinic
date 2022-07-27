625e08a5870fdfe5af028f236cae55f8647f2c20
Davide Fazio <107030098+DavideFazioWeav@users.noreply.github.com>
2022-07-27 10:21:19 +0200

    First Commit
    This is a starter template created using Spring Initializer for the Develhope Clinic project.

---
+++ src/test/DevelhopeClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package it.develhope.team1.clinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevelhopeClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
