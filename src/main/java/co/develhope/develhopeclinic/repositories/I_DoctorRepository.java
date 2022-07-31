57f1b915374482466849f6adf40b55ca5c3406da
Davide Fazio <drumprog@gmail.com>
2022-07-31 14:55:30 +0200

    Added @Repository at I_DoctorRepository

--- src/main/java/co/develhope/develhopeclinic/repositories/I_DoctorRepository.java
+++ src/main/java/co/develhope/develhopeclinic/repositories/I_DoctorRepository.java
@@ -2,7 +2,9 @@ package co.develhope.develhopeclinic.repositories;
 
 import co.develhope.develhopeclinic.entities.Doctor;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface I_DoctorRepository extends JpaRepository<Doctor,Integer> {
 
     Doctor findByName(String name);
