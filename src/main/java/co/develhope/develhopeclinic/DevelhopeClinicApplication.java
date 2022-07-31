82de34df1b395f4506749cda6b9e47bc17ff9c64
Davide Fazio <drumprog@gmail.com>
2022-07-31 14:30:49 +0200

    Fixed a Package error that didn't allowed the application to start properly, added annotations missing from classes.

--- src/main/java/co/develhope/develhopeclinic/DevelhopeClinicApplication.java
+++ src/main/java/co/develhope/develhopeclinic/DevelhopeClinicApplication.java
@@ -1,6 +1,9 @@
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+
+@ComponentScan(basePackages = {"co.develhope.develhopeclinic.DevelhopeClinicApplication"})
 @SpringBootApplication
 public class DevelhopeClinicApplication {
 
