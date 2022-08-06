79bd2385b38eaba48c1ba70f22aefc39f2c442cc
Angelo Zammataro <juba.kem@gmail.com>
2022-08-06 16:19:31 +0200

    Create method updateSingleAppointment in AppointmentService - testing all CRUD : OK

--- src/main/java/co/develhope/develhopeclinic/services/AppointmentService.java
+++ src/main/java/co/develhope/develhopeclinic/services/AppointmentService.java
@@ -4,6 +4,9 @@ import co.develhope.develhopeclinic.entities.Appointment;
 import co.develhope.develhopeclinic.repositories.I_AppointmentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 import java.util.Optional;
@@ -26,6 +29,17 @@ public class AppointmentService {
         return  appointmentRepository.findById(id).orElse(null);
     }
 
+    public Appointment updateSingleAppointment(int id, Appointment appointment){
+        appointment.setId(id);
+        Appointment newAppointment = appointmentRepository.saveAndFlush(appointment);
+        boolean existsById = appointmentRepository.existsById(id);
+        if(existsById){
+            //modify a single Appointment - if the id is in the db
+            return appointment;
+        }
+        //returns an empty Appointment if the id is NOT in the db
+        return new Appointment();
+    }
     public String deleteAppointment (int id){
         appointmentRepository.deleteById(id);
         return "Appointment n° " + id + " has been removed!";
